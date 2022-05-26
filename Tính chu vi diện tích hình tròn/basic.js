09c10f3587f27226066f6a2067dbd1fa2a05dba7
HongTheKiet <105725702+HongTheKiet@users.noreply.github.com>
2022-05-26 16:34:28 +0700

    bai thuc hanh/bai tap bien, kieu du lieu, toan tu

---
+++ Tính chu vi diện tích hình tròn/basic.js
@@ -0,0 +1,7 @@
+let pi = 3.14;
+let R = prompt ('Nhập bán kính hình tròn : ')
+document.write ("Đường kính hình tròn là : " + R*2)
+document.write ("<br/>")
+document.write ("Chu vi hình tròn là : " + 2*R*pi)
+document.write ("<br/>")
+document.write ("Diện tích hình tròn là : " + R**2*pi)
---
+++ Tính lãi suất ngân hàng/basic.js
@@ -0,0 +1,6 @@
+let tienVay = prompt("Số tiền vay : ");
+let laiXuat = 6.9;
+let soNam = prompt ("Số năm sẽ vay : ");
+let tienLai = tienVay * laiXuat / 100;
+
+document.write (tienVay + (soNam * tienLai));
